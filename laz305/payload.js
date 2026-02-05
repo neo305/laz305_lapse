c45f89b0eb78eb8179a95f4addd9fd60b79b661a
neo305 <laz305@gmail.com>
2026-02-04 20:42:03 -0500

    Update Goldhen
    2.4b18.8

--- laz305/payload.js
+++ laz305/payload.js
@@ -95,8 +95,8 @@ link.click();
 }
 
 function load_platinum(){
-LoadedMSG="GoldHEN v2.4b18.3 Loaded !!!";
-PLfile = "goldhen_2.4b18.3.bin";
+LoadedMSG="GoldHEN v2.4b18.8 Loaded !!!";
+PLfile = "goldhen_2.4b18.8.bin";
 toogle_payload();
 } 
 
